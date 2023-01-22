bf4ec4eab31ec8bb1808ecfa40b70beead1688dd
khj <kim37171@cuk.edu>
2023-01-22 17:18:07 +0900

    initial commit

---
+++ src/main/java/personel/prac/designpattern/HelloApplication.java
@@ -0,0 +1,9 @@
+package personel.prac.designpattern;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
