3be3834987a7b9b20647b5358754591456ccf813
khj <kim37171@cuk.edu>
2023-02-15 23:45:35 +0900

    SOLID

---
+++ src/main/java/personel/prac/designpattern/solid/Solid.java
@@ -0,0 +1,16 @@
+package personel.prac.designpattern.solid;
+
+public class Solid {
+	/**
+	 * SOLID
+	 * Single Responsibility Principle
+	 *
+	 * Open Closed Principle
+	 *
+	 * Liskov Substitution Principle
+	 *
+	 * Interface Segregation Principle
+	 *
+	 * Dependency Inversion Principle
+	 * */
+}
