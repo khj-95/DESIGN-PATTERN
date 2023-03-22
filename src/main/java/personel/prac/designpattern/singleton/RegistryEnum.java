3339ba61470166f7925f013982a1ffc0c3532b2d
khj <kim37171@cuk.edu>
2023-03-22 21:37:03 +0900

    Singleton using Enum

---
+++ src/main/java/personel/prac/designpattern/singleton/RegistryEnum.java
@@ -0,0 +1,12 @@
+package personel.prac.designpattern.singleton;
+
+/**
+ * serialization/deserialization 이슈 존재
+ * */
+public enum RegistryEnum {
+    INSTANCE;
+
+    public void getConfiguration() {
+
+    }
+}
